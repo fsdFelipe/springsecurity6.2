40193b291210d248d1b73e344dc5480cc7c61842
fsdFelipe <devfsdfelipe@gmail.com>
2024-03-27 16:54:30 -0300

    Criptografando a senha

--- src/main/java/com/app/auth/config/SecurityConfig.java
+++ src/main/java/com/app/auth/config/SecurityConfig.java
@@ -6,26 +6,26 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-	
-	private static final String[] PUBLIC_MATHERS = {
-			"/h2-console/**",
-			"/usuarios/**",
-	};
-	
-	 @Bean
-	    SecurityFilterChain apiSecurityFilterChain(HttpSecurity httpSecurity) throws Exception{
-	        return httpSecurity
-	                .csrf(AbstractHttpConfigurer::disable)
-	                .headers(headers -> headers.disable())
-	                .authorizeHttpRequests(auth -> auth
-	                		.requestMatchers(HttpMethod.GET, PUBLIC_MATHERS).permitAll()
-	                		.requestMatchers(HttpMethod.POST, PUBLIC_MATHERS).permitAll()
-	                		.anyRequest().authenticated())
-	                .build();
-	    }
+
+	private static final String[] PUBLIC_MATHERS = { "/h2-console/**", "/usuarios/**", };
+
+	@Bean
+	SecurityFilterChain apiSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		return httpSecurity.csrf(AbstractHttpConfigurer::disable).headers(headers -> headers.disable())
+				.authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.GET, PUBLIC_MATHERS).permitAll()
+						.requestMatchers(HttpMethod.POST, PUBLIC_MATHERS).permitAll().anyRequest().authenticated())
+				.build();
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
