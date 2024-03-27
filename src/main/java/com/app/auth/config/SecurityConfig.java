8f0193363097f6ab6afbb8df8d490c70fd68c9bb
fsdFelipe <devfsdfelipe@gmail.com>
2024-03-27 17:07:32 -0300

    Implementando spring security – UserDetais

--- src/main/java/com/app/auth/config/SecurityConfig.java
+++ src/main/java/com/app/auth/config/SecurityConfig.java
@@ -13,19 +13,26 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-	private static final String[] PUBLIC_MATHERS = { "/h2-console/**", "/usuarios/**", };
-
-	@Bean
-	SecurityFilterChain apiSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
-		return httpSecurity.csrf(AbstractHttpConfigurer::disable).headers(headers -> headers.disable())
-				.authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.GET, PUBLIC_MATHERS).permitAll()
-						.requestMatchers(HttpMethod.POST, PUBLIC_MATHERS).permitAll().anyRequest().authenticated())
-				.build();
-	}
-
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+	
+	private static final String[] PUBLIC_MATHERS = {
+			"/h2-console/**",
+			"/usuarios/**",
+	};
+	
+	 @Bean
+	    SecurityFilterChain apiSecurityFilterChain(HttpSecurity httpSecurity) throws Exception{
+	        return httpSecurity
+	                .csrf(AbstractHttpConfigurer::disable)
+	                .headers(headers -> headers.disable())
+	                .authorizeHttpRequests(auth -> auth
+	                		.requestMatchers(HttpMethod.GET, PUBLIC_MATHERS).permitAll()
+	                		.requestMatchers(HttpMethod.POST, PUBLIC_MATHERS).permitAll()
+	                		.anyRequest().authenticated())
+	                .build();
+	    }
+	 
+		@Bean
+		PasswordEncoder passwordEncoder() {
+			return new BCryptPasswordEncoder();
+		}
 }
