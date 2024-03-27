460ff5e5e71498568c5137df02a5248a8ca7e6aa
fsdFelipe <devfsdfelipe@gmail.com>
2024-03-27 16:02:09 -0300

    Iniciando a implementação do spring security

---
+++ src/main/java/com/app/auth/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.app.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	private static final String[] PUBLIC_MATHERS = {
+			"/h2-console/**",
+			"/usuarios/**",
+	};
+	
+	 @Bean
+	    SecurityFilterChain apiSecurityFilterChain(HttpSecurity httpSecurity) throws Exception{
+	        return httpSecurity
+	                .csrf(AbstractHttpConfigurer::disable)
+	                .headers(headers -> headers.disable())
+	                .authorizeHttpRequests(auth -> auth
+	                		.requestMatchers(HttpMethod.GET, PUBLIC_MATHERS).permitAll()
+	                		.requestMatchers(HttpMethod.POST, PUBLIC_MATHERS).permitAll()
+	                		.anyRequest().authenticated())
+	                .build();
+	    }
+}
